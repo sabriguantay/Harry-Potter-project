edeff4223b6226a19b18449decbfb252bdb030e1
sabriguantay <52327724+sabriguantay@users.noreply.github.com>
2019-08-27 18:04:44 -0300

    Add files via upload

---
+++ app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Bienvenidos a Harry Potter Game!\n"
+        + "Jugador 1 elija su personaje:\n" 
+        + "1 = ");
+
+
+}
+    }
